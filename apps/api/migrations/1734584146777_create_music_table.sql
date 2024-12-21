93e59a34e3dcc7ccd02de9324b5e5cbd748d46cd
Prashun Chitrakar <prashunchi@gmail.com>
2024-12-21 12:58:29 +0545

    feat(music): add id column in schema

--- apps/api/migrations/1734584146777_create_music_table.sql
+++ apps/api/migrations/1734584146777_create_music_table.sql
@@ -1,6 +1,6 @@
 CREATE TYPE GENRE AS ENUM ('rnb', 'country', 'classic', 'rock', 'jazz');
-
 CREATE TABLE music (
+    id SERIAL PRIMARY KEY,
     artist_id int NOT NULL REFERENCES artists(id),
     title varchar(255) NOT NULL,
     album_name varchar(255),
@@ -8,5 +8,4 @@ CREATE TABLE music (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE INDEX idx_music_artist_id ON music(artist_id);
+CREATE INDEX idx_music_artist_id ON music(artist_id)
